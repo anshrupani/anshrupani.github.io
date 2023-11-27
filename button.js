b18b2b8134e4678e34d2de7c35c093f2f07ee078
Ansh Rupani <anshrupani18@gmail.com>
2023-11-27 23:48:07 +0100

    Add select field and remove background color

--- button.js
+++ button.js
@@ -18,165 +18,6 @@ $(document).on("click"," .cross", function(){
   $('#formModal').removeClass('modal-show');
 });
 
-// $(function() {
-
-//   $("#newModalForm").validate({
-//     rules: {
-//       firstname: {
-//         required: true,
-//         minlength: 8
-//       },
-//       email: "required"
-//     },
-//     messages: {
-//       firstname: {
-//         required: "Please enter some data",
-//         minlength: "Your data must be at least 8 characters"
-//       },
-//       email: "Please provide some data"
-//     }
-//   });
-// });
-
-
-
-// $('#newModalForm').on('show.bs.modal', function () {
-//   $(document).on('click', function submitform() {
-//     var f = document.getElementsByTagName('form')[0];
-//     var pr = document.createElement("p");
-//     if(f.checkValidity()) {
-//       f.submit();
-//     } else {
-//       // alert(document.getElementById('email').validationMessage);
-//       pr.textContent = "min 5 characters";
-//       document.body.appendChild(pr);
-      
-//     }
-//   });
-// })
-
-
-// function submitform() {
-//   var f = document.getElementsByTagName('form')[0];
-//   var pr = document.createElement("p");
-//   if(f.checkValidity()) {
-//     f.submit();
-//   } else {
-//     // alert(document.getElementById('email').validationMessage);
-//     pr.textContent = "min 5 characters";
-//     document.body.appendChild(pr);
-    
-//   }
-// }
-
-
-
-// $('#newModalForm').on('submit', function submitform() {
-//   var f = document.getElementsByTagName('form')[0];
-//   var pr = document.createElement("p");
-//   if(f.checkValidity()) {
-//     f.submit();
-//   } else {
-//     // alert(document.getElementById('email').validationMessage);
-//     pr.textContent = "min 5 characters";
-//     document.body.appendChild(pr);
-    
-//   }
-// });
-
-
-
-// function submitform() {
-//   var f = document.getElementsByTagName('form')[0];
-//   var pr = document.createElement("p");
-//   if(f.checkValidity()) {
-//     f.submit();
-//   } else {
-//     // alert(document.getElementById('email').validationMessage);
-//     pr.textContent = "min 5 characters";
-//     document.body.appendChild(pr);
-    
-//   }
-// }
-
-// $('body, html').on('click','.submit', function(e) {
-//   e.preventDefault();
-//   var f = document.getElementsByTagName('form')[0];
-//   var pr = document.createElement("p");
-//   if(f.checkValidity()) {
-//     f.submit();
-//   } else {
-//     // alert(document.getElementById('email').validationMessage);
-//     pr.textContent = "min 5 characters";
-//     document.body.appendChild(pr);
-    
-//   }
-
-//   // alert('hello world');
-// });
-
-// if (!$('form')[0].checkValidity()) {
-//   $('form').find('input[type="submit"]').click();
-// }
-
-// $('#newModalForm').on('box-modal', function () {
-//   $('.classFromElement').on('click', function (e) {
-//       e.preventDefault();
-//       alert('hello world');
-//   });
-// })
-
-
-
-
-// $(function() {
-
-//   $("#newModalForm").validate();
-// });
-
-
-
-
-
-// $(document).ready(function() {
-//   $("#newModalForm").validate({
-//     rules: {
-//       email : {
-//         required: true
-//       }
-//     },
-//     messages : {
-//       email: "Please enter Name"
-//     },
-//     errorElement: "div",
-//       errorPlacement: function ( error, element ) {
-//           error.addClass( "invalid-feedback" );
-//           error.insertAfter( element );
-//       },
-//     highlight: function(element) {
-//       $(element).removeClass('is-valid').addClass('is-invalid');
-//     },
-//     unhighlight: function(element) {
-//       $(element).removeClass('is-invalid').addClass('is-valid');
-//     }
-//   });
-// });
-
-// $(document).on("click"," .submit", function(){
-//   $('body').removeClass('active-modal');
-//   $('.box-modal').removeClass('modal-show');
-//   $('body').addClass('active-modal');
-//   $('.done-modal').addClass('modal-show');
-// });
-
-// $(document).ready(function(){
-//   $('.form').on('submit', function(){
-//       swal("Title", "Message Content", "success", {
-// button: "Ok",
-// });
-//   });
-// });
-
 $('#newModalForm').on('submit', function(e){
   $('#formModal').removeClass('modal-show');
   $('#myModal').addClass('modal-show');
@@ -188,31 +29,20 @@ $('#newModalForm').on('submit', function(e){
 });
 
 
-
-// $("input[type='text']").bind('focus', function() {
-//    $(this).css('background-color', 'none');
-// });
-
-// $(document).ready(function() {
-//   setTimeout(function() {
-//       $('html, body').animate({
-//          scrollTop: $('#bglast').offset().top 
-//       },2000);
-//   }, 500);
-// });
-
-// function pageScroll() {
-//   window.scrollBy(0,1);
-//   scrolldelay = setTimeout(pageScroll,10);
-// }
-// setTimeout(function () {
-//   window.scrollTo(0, 300);
-// },200);
-
 $('#bg').animate({ scrollTop: $('#bglast').offset().top  }, 2000);
 $('#bg').animate({ scrollTop: $('#bgfirst').offset().top  }, 750);
 
 
-// $(document).ready(function(){
-//   $('body,html').animate({scrollTop: 300}, 10); 
-// });
+// Put this script in header or above select element
+function check(elem) {
+  // use one of possible conditions
+  // if (elem.value == 'Other')
+  if (elem.selectedIndex == 6) {
+      document.getElementById("other-div").style.display = 'block';
+      document.getElementById("other-input").required = true;
+      // document.getElementById("other-div").setAttribute('required','required');
+      // $("#other-div").prop('required', true)
+  } else {
+      document.getElementById("other-div").style.display = 'none';
+  }
+}
